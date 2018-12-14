ef3d4875cd6a4baa7f7b1ceb6488ea2a71af4e2e
Rizwan MD <rizwan.b@technovert.com>
2018-12-14 19:14:23 +0530

    Changes made in ContactDetails Model

--- Technovert/Models/ContactDetails.cs
+++ Technovert/Models/ContactDetails.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,23 @@ namespace Technovert.Models
 	public class ContactDetails
 	{
 		public int ID { get; set; }
+
+		[Required]
 		public string Name { get; set; }
+
 		public string Email { get; set; }
-		public int Mobile { get; set; }
-		public int Landline { get; set; }
+
+		[Required]
+		[RegularExpression(@"^[6-9]+[0-9""'\s-]*$")]
+		[StringLength(10)]
+		public string Mobile { get; set; }
+
+		[RegularExpression(@"^[0-9""'\s-]*$")]
+		[StringLength(10)]
+		public string Landline { get; set; }
 		public string Website { get; set; }
+
+		[Required]
 		public string Address { get; set; }
 	}
 }
